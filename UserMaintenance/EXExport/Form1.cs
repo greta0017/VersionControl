8dba8e4c9e80ca7fc1224cee05d427f294c373b4
greta00178@gmail.com <greta00178@gmail.com>
2021-10-08 19:25:07 +0200

    4gyak02

---
+++ UserMaintenance/EXExport/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace EXExport
+{
+    public partial class Form1 : Form
+    {
+        
+        List<Flat> li; 
+        RealEstateEntities real = new RealEstateEntities();  //példányosítás
+          
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Console.WriteLine(li.Count);
+            // li = new List<Flat>(); //zárójel!!!
+
+            LoadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void LoadData()
+        {
+            li = real.Flats.ToList();
+        }
+
+    }
+}
