38d5fee7d7d9ead5d29eb69e89a91a2ea1d6ea10
greta00178@gmail.com <greta00178@gmail.com>
2021-10-08 19:53:39 +0200

    4gyak03

--- UserMaintenance/EXExport/Form1.cs
+++ UserMaintenance/EXExport/Form1.cs
@@ -14,10 +14,13 @@ namespace EXExport
 {
     public partial class Form1 : Form
     {
-        
-        List<Flat> li; 
+
+        List<Flat> li;
         RealEstateEntities real = new RealEstateEntities();  //példányosítás
-          
+
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
 
         public Form1()
         {
@@ -26,6 +29,7 @@ namespace EXExport
             // li = new List<Flat>(); //zárójel!!!
 
             LoadData();
+            CreateExcel();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -37,6 +41,108 @@ namespace EXExport
         {
             li = real.Flats.ToList();
         }
+
+        void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                // CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
+        }
+
+        void CreateTable()
+        {
+            string[] headers = new string[] {
+             "Kód",
+             "Eladó",
+             "Oldal",
+             "Kerület",
+             "Lift",
+             "Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"};
+
+
+            object[,] values = new object[li.Count, headers.Length];
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+               
+
+            }
+
+            int counter = 0;
+            foreach (Flat f in li)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 8] = "";
+                counter++;
+            }
+
+            xlSheet.get_Range(  //range-terület visszakapása (bal felső saroktól jobb alsóig)
+             GetCell(2, 1), //excel sor,oszlop 2.1
+             GetCell(1 + values.GetLength(0), values.GetLength(1))
+             ).Value2 = values; //kövi órán átbeszélni
+
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+
 
     }
 }
