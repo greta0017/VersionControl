632b78770a14965a5585446c3a5a3a0a56e0e9e7
greta00178@gmail.com <greta00178@gmail.com>
2021-10-21 19:11:48 +0200

    week62

---
+++ UserMaintenance/Week6/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week6
+{
+    public partial class Form1 : Form
+    {
+        List<Tick> ticks;
+        PortfolioEntities port = new PortfolioEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            ticks = port.Tick.ToList();
+            dataGridView1.DataSource = ticks; //select * from tick
+
+        }
+    }
+}
