f232dc52f7a32b085f3f4ceae6c08f8113a00fa3
greta00178@gmail.com <greta00178@gmail.com>
2021-10-21 19:19:03 +0200

    week63

--- UserMaintenance/Week6/Form1.cs
+++ UserMaintenance/Week6/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week6.Entities;
 
 namespace Week6
 {
@@ -14,12 +15,23 @@ namespace Week6
     {
         List<Tick> ticks;
         PortfolioEntities port = new PortfolioEntities();
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
             ticks = port.Tick.ToList();
             dataGridView1.DataSource = ticks; //select * from tick
+            CreatePortfolio();
+        }
 
+        void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
