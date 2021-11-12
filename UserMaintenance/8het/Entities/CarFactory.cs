8e621114296bc02b0b0a9efd824aed44392520b9
greta00178@gmail.com <greta00178@gmail.com>
2021-11-12 20:54:55 +0100

    8/4

--- UserMaintenance/8het/Entities/CarFactory.cs
+++ UserMaintenance/8het/Entities/CarFactory.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace _8het.Entities
 {
-    public class BallFactory : IToyFactory 
+    public class CarFactory: IToyFactory
     {
         public Toy CreateNew()
         {
