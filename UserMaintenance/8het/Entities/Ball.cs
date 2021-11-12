0917f4ff69eb039a2302f9a310ede1453ea364fb
greta00178@gmail.com <greta00178@gmail.com>
2021-11-12 20:27:55 +0100

    8/2

---
+++ UserMaintenance/8het/Entities/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8het.Entities
+{
+    public class Ball : Label
+    {
+       public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = 50;
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        void DrawImage(Graphics graph) 
+        {
+            graph.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            this.Left++;
+        }
+    }
+}
--- UserMaintenance/8het/Form1.cs
+++ UserMaintenance/8het/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _8het.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,49 @@ namespace _8het
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory factory;
+
+        public BallFactory Factory
+        {
+            get { return factory; }
+            set { factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            mainPanel.Controls.Add(ball);
+            ball.Left = ball.Width * (-1);
+            
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var item in _balls)
+            {
+                item.MoveBall();
+                if (item.Left>maxPosition)
+                {
+                    maxPosition = item.Left;
+                }
+            }
+            if (maxPosition>1000)
+            {
+                Ball ba = _balls.First(); //ua-> Ball ba =_balls[0];
+                _balls.Remove(ba);
+                //form vezérlői - controls, feladat megfogalmazása rossz
+                mainPanel.Controls.Remove(ba);
+            }
         }
     }
 }
