f2892fba899f1f3be09cf455f05699c5ea346530
greta00178@gmail.com <greta00178@gmail.com>
2021-12-02 21:01:30 +0100

    2

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+
+        [Test]
+        [TestCase("asdfgh", false)]
+        [TestCase("asdfgh@gmail", false)]
+        [TestCase("asdfghgmail.com", false)]
+        [TestCase("asdfgh@gmail.com", true)]
+
+
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            var accountC = new AccountController(); //email validálás rész
+            var actualR = accountC.ValidateEmail(email); 
+            Assert.AreEqual(expectedResult,actualR);
+        }
+
+        [TestCase("asdfgh", false)]//fv felé rakni a teszteseteket, arra fog vonatkozni
+        [TestCase("ASDFGHJ", false)]
+        [TestCase("asdfghs", false)]
+        [TestCase("asd", false)]
+        [TestCase("asdfghASD20", true)]
+
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountC = new AccountController();
+            var actualR = accountC.ValidatePassword(password);
+            Assert.AreEqual(expectedResult, actualR);
+        }
+    }
+}
---
+++ UnitTestExample/UnitTestExample.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec3ae484-9b30-4384-9c28-496daebf37c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
