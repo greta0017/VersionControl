ad9f94163fd10dc7301d8e845049da04ea1a636c
greta00178@gmail.com <greta00178@gmail.com>
2021-12-03 17:05:03 +0100

    10/2

---
+++ UserMaintenance/Tizedik/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tizedik.Entities
+{
+    public class DeathProbability
+    {
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public double P { get; set; }
+
+    }
+}
---
+++ UserMaintenance/Tizedik/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tizedik.Entities;
+
+namespace Tizedik
+{
+    public partial class Form1 : Form
+    {
+
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public List<Person> Elso(string a)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(a, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+
+            return population;
+        }
+
+        public List<BirthProbability> Masodik(string a)
+        {
+            List<BirthProbability> population = new List<BirthProbability>();
+
+            using (StreamReader sr = new StreamReader(a, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new BirthProbability()
+                    {
+                        Age = int.Parse(line[0]),
+                        NbrOfChildren = int.Parse(line[1]),
+                        P = double.Parse(line[2])
+                    });
+                }
+            }
+
+            return population;
+        }
+        public List<DeathProbability> Harmadik(string a)
+        {
+            List<DeathProbability> population = new List<DeathProbability>();
+
+            using (StreamReader sr = new StreamReader(a, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new DeathProbability()
+                    {
+                        Age = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        P = double.Parse(line[2])
+                    });
+                }
+            }
+
+            return population;
+        }
+
+
+
+    }
+}
